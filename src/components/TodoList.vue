0c648cdd058d0f6ca98074b897cadd8ec0d84fb5
Daan <daandenhoed@gmail.com>
2023-04-03 17:31:02 +0200

    getTodos

---
+++ src/components/TodoList.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <h1>Todos</h1>
+        <ul>
+            <li v-for="todo in todos" :key="todo.id">
+                {{ todo.title }}
+            </li>
+        </ul>
+    </div>
+</template>
+  
+<script>
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            todos: [],
+        };
+    },
+    created() {
+        this.getTodos();
+    },
+    methods: {
+        async getTodos() {
+            try {
+                const response = await axios.get('http://localhost:8080/todos');
+                this.todos = response.data;
+            } catch (error) {
+                console.error(error);
+            }
+        },
+    },
+};
+</script>
