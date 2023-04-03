1f6b790debb40cd7a1c3b122de6642b7061a6545
Daan <daandenhoed@gmail.com>
2023-04-03 18:24:25 +0200

    add todo

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,33 @@
 <template>
   <div id="app">
     <TodoList />
+    <TodoForm @add-todo="addTodo" />
   </div>
 </template>
 
 <script>
 import TodoList from './components/TodoList.vue'
+import TodoForm from './components/TodoForm.vue'
+import { response } from 'express';
 
 export default {
   name: 'App',
   components: {
-   TodoList
+   TodoList,
+   TodoForm,
+  },
+  methods: {
+    async addTodo(todo) {
+      const reponse = await fetch('http://localhost:8080/todos', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(todo),
+      });
+      const data = await response.json();
+      console.log(data);
+    },
   },
 };
 </script>
---
+++ src/components/TodoForm.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <input type="text" v-model="title" placeholder="Add a new todo">
+        <button @click="addTodo">Add</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            title: '',
+        };
+    },
+    methods: {
+        addTodo() {
+            const todo = {
+                title: this.title,
+            };
+            this.$emit('add-todo', todo);
+            this.title = '';
+        },
+    },
+};
+</script>
